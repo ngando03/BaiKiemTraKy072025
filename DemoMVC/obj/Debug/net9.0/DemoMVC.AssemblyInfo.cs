79d8a83b93932ed966c47e24d8b4cb103721c5af
ngando03 <dongan03102003@gmail.com>
2025-07-22 09:17:55 +0700

    BaiKiemTraKy072025

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124a6bc662aea61b4d37a8435813767961f80b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
